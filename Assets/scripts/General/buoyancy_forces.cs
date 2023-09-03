87c9c3a6e02d46fcb83f5c90e1bb48b8e8ee7bf8
XingjianL <lixingjian2002@gmail.com>
2023-09-02 23:18:42 -0400

    force vector visualization

--- Assets/scripts/General/buoyancy_forces.cs
+++ Assets/scripts/General/buoyancy_forces.cs
@@ -8,12 +8,20 @@ public class buoyancy_forces : MonoBehaviour
     public float waterDensity = 1.025f; // kg/L
     public float volumeDisplaced = 0;
     Rigidbody m_Rigidbody;
+    public bool forceVisual = true;
+    public Material forceVisualMaterial;
     [HideInInspector] public bool underwater;
 
     // Start is called before the first frame update
     void Start()
     {
         m_Rigidbody = GetComponent<Rigidbody>();
+        if (forceVisualMaterial != null){
+            transform.Find("env_force_visual").gameObject.GetComponent<MeshRenderer>().material = forceVisualMaterial;
+        }
+        if (!forceVisual){
+            transform.Find("env_force_visual").gameObject.GetComponent<MeshRenderer>().enabled = false;
+        }
     }
 
     // Update is called once per frame
@@ -25,16 +33,25 @@ public class buoyancy_forces : MonoBehaviour
     void FixedUpdate(){
         //print(m_Rigidbody.centerOfMass);
         float difference = transform.position.y + m_Rigidbody.centerOfMass.y - waterHeight;
+        float buoyancy_force = 0;
         if (difference < 0){
             underwater = true;
         } else {
             underwater = false;
         }
         if (underwater){
-            float buoyancy_force = waterDensity * Physics.gravity.y * volumeDisplaced; // F = density * gravity accel * Volume
+            buoyancy_force = waterDensity * Physics.gravity.y * volumeDisplaced; // F = density * gravity accel * Volume
             //print(buoyancy_force);
             //print(Physics.gravity);
             m_Rigidbody.AddForce(new Vector3(0, -buoyancy_force, 0));
+        }
+        if (forceVisual){
+            float environment_force_diff = (-buoyancy_force + m_Rigidbody.mass * Physics.gravity.y)/Mathf.Abs(Physics.gravity.y);
+            print(environment_force_diff);
+            var force_visual = transform.Find("env_force_visual");
+            if (force_visual != null){
+                force_visual.localScale = new Vector3(environment_force_diff/10,environment_force_diff/10,environment_force_diff/2);
+            }
         }
     }
 }
--- Assets/scripts/Robot/RobotForce.cs
+++ Assets/scripts/Robot/RobotForce.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class RobotForce : MonoBehaviour
 {
     public GameObject[] thrusters;
     public float strength = 2.36f;
     public float SubmergeDepth;
-
+    public bool forceVisual = true;
+    public Material forceVisualMaterial;
     Rigidbody m_rigidBody;
+    Vector3[] thruster_directions;
+    const float KGF_TO_N = 9.80665f;
+    const float MAX_FORCE = 2.36f*KGF_TO_N;
     // Start is called before the first frame update
     void Start()
     {
         m_rigidBody = GetComponent<Rigidbody>();
+        if (forceVisualMaterial != null){
+            foreach (var thruster in thrusters) {
+                thruster.transform.Find("force_visual").gameObject.GetComponent<MeshRenderer>().material = forceVisualMaterial;
+            }
+        }
+        if (!forceVisual){
+            foreach (var thruster in thrusters) {
+                thruster.transform.Find("force_visual").gameObject.GetComponent<MeshRenderer>().enabled = false;
+            }
+        }
+        stop_thrust();
     }
 
     // Update is called once per frame
@@ -20,34 +34,66 @@ public class RobotForce : MonoBehaviour
     {
         
     }
-    void submerge_force(float force){
-        var forceDirection = new Vector3(0, force, 0);
-        m_rigidBody.AddForceAtPosition(forceDirection, thrusters[0].transform.position);
-        m_rigidBody.AddForceAtPosition(forceDirection, thrusters[1].transform.position);
-        m_rigidBody.AddForceAtPosition(forceDirection, thrusters[2].transform.position);
-        m_rigidBody.AddForceAtPosition(forceDirection, thrusters[3].transform.position);
-        //if (transform.rotation.eulerAngles.z/180 > 0) {
-        //    m_rigidBody.AddForceAtPosition(forceDirection/transform.rotation.eulerAngles.z, thrusters[1].transform.position);
-        //    m_rigidBody.AddForceAtPosition(forceDirection/transform.rotation.eulerAngles.z, thrusters[3].transform.position);
-        //}
-        //print(force);
+    void stop_thrust(){
+        add_thruster_force(0, 0);
+        add_thruster_force(1, 0);
+        add_thruster_force(2, 0);
+        add_thruster_force(3, 0);
+        add_thruster_force(4, 0);
+        add_thruster_force(5, 0);
+        add_thruster_force(6, 0);
+        add_thruster_force(7, 0);
     }
-    void forward_force(float force){
-        var forceDirection_front = new Vector3(force, 0, 0);
-        var forceDirection_back = new Vector3(force, 0, 0);
-        m_rigidBody.AddForceAtPosition(forceDirection_front, thrusters[4].transform.position);
-        m_rigidBody.AddForceAtPosition(forceDirection_front, thrusters[5].transform.position);
-        m_rigidBody.AddForceAtPosition(forceDirection_back, thrusters[6].transform.position);
-        m_rigidBody.AddForceAtPosition(forceDirection_back, thrusters[7].transform.position);
+    void submerge(float force){
+        add_thruster_force(0, -force);
+        add_thruster_force(1, -force);
+        add_thruster_force(2, -force);
+        add_thruster_force(3, -force);   
     }
+    void forward(float force){
+        add_thruster_force(4, force);
+        add_thruster_force(5, force);
+        add_thruster_force(6, -force);
+        add_thruster_force(7, -force);
+    }
+
+    /// force: positive CCW
+    void spin(float force){
+        add_thruster_force(4, force );
+        add_thruster_force(5, -force);
+        add_thruster_force(6, force );
+        add_thruster_force(7, -force);
+    }
+    void add_thruster_force(int id, float force){
+        if (force < 0) {
+            force = force * 1.85f / 2.36f; // max forward/reverse spec sheet: https://www.thingbits.in/products/t100-thruster-no-esc
+        }
+        if (force > MAX_FORCE) {
+            force = MAX_FORCE;
+        }
+        m_rigidBody.AddForceAtPosition(force * thrusters[id].transform.forward, thrusters[id].transform.position);
+        if (forceVisual) {
+            Transform force_visual = thrusters[id].transform.Find("force_visual");
+            if (force_visual != null) {
+                //print(force_visual.position);
+                //if (force < 0){
+                //    force_visual.localRotation = Quaternion.Euler(180,0,0);
+                //}
+                //force_visual.localPosition = new Vector3(0,0,(force/2)/MAX_FORCE);
+                var arrow_scale = (force)/MAX_FORCE;
+                force_visual.localScale = new Vector3(arrow_scale/5,arrow_scale/5,arrow_scale);
+            }
+        }
+    }
+
     void FixedUpdate(){
         //print(transform.rotation.eulerAngles); // (roll,yaw,pitch)
         //SubmergeDepth = GetComponent<buoyancy_forces>().underwater;
         if (transform.position.y <= SubmergeDepth) {
-            submerge_force(0);
-            forward_force(strength * m_rigidBody.mass/4);// * m_rigidBody.mass);
+            submerge(0);            
+            spin(-strength * KGF_TO_N);
         } else {
-            submerge_force(-strength * m_rigidBody.mass);
+            submerge(strength * KGF_TO_N);
         }
         //forward_force(strength * m_rigidBody.mass);
 
