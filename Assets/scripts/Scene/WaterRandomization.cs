a3f8237239e3cfc50fc071afa67478f790c37aeb
Xingjian Li <lixingjian2002@gmail.com>
2023-09-25 21:13:30 -0400

    water color randomization

---
+++ Assets/scripts/Scene/WaterRandomization.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.HighDefinition;
+public class WaterRandomization : MonoBehaviour
+{
+    public WaterSurface waterScript;
+    private const int BG_SUM_MAX = 224;
+    private const int BG_SUM_MIN = 140;
+    private const int BG_MAX = (BG_SUM_MAX + BG_SUM_MIN) / 4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(waterScript.scatteringColor);
+        print(waterScript.underWaterScatteringColor);
+    }
+
+    // Update is called once per frame
+    int count = 0;
+    void Update()
+    {
+        count += 1;
+        if (count > 50) {
+            RandomScatteringColor();
+            count = 0;
+        }
+        //print(count);
+    }
+    void RandomScatteringColor(){
+        int sum_color = Random.Range(BG_SUM_MIN, BG_SUM_MAX);
+        int blue = Random.Range(BG_SUM_MIN/2, BG_MAX);
+        int green = sum_color - blue;
+        waterScript.scatteringColor.b = blue/255.0f;
+        waterScript.scatteringColor.g = green/255.0f;
+        float brightness = Random.Range(0.3f, 1.0f);
+        waterScript.scatteringColor *= brightness;
+    }
+}
