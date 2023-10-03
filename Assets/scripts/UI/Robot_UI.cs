925629914a452d8bb7fad6771e0b23dc4a4ea17b
Xingjian Li <lixingjian2002@gmail.com>
2023-10-02 21:02:18 -0400

    UI setup

--- Assets/scripts/UI/Robot_UI.cs
+++ Assets/scripts/UI/Robot_UI.cs
@@ -2,31 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-//using TMPro;
-
-public class TCP_UI : MonoBehaviour
+public class Robot_UI : MonoBehaviour
 {
-    public TCPServer tcp_script;
+    public GameObject Robot;
+    TCPServer tcp_script;
     public TMPro.TMP_InputField IPaddr;
     public TMPro.TMP_InputField Port;
     public Toggle runServer;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        //print(IPaddr.text);
-        //print(Port.text);
+    void Start(){
+        refresh();
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void refresh(){
+        tcp_script = Robot.GetComponent<TCPServer>();
     }
     public void configTCPScript(){
         print("UI TOGGLE");
         tcp_script.IPAddr = IPaddr.text;
         tcp_script.port = int.Parse(Port.text);
         tcp_script.runServer = runServer.isOn;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
