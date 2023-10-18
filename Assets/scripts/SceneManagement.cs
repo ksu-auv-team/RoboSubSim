0e03a39c4b6bbcb0b3f3f8581e09f0eab0c69279
XingjianL <lixingjian2002@gmail.com>
2023-10-17 20:54:55 -0400

    scene manager object

--- Assets/scripts/SceneManagement.cs
+++ Assets/scripts/SceneManagement.cs
@@ -4,9 +4,38 @@ using UnityEngine;
 
 public class SceneManagement : MonoBehaviour
 {
+    string[] tagNames = {"Robot", "Pool", "2023Objective", "Environment"};
+    List<GameObject[]> gameObjects = new List<GameObject[]>();
+    int tagSelects;
+    int[] objectSelects;
     // Start is called before the first frame update
     void Start()
     {
+        registerAllSceneObjects();
+        displayAllRegisteredObjects();
+        
+    }
+
+    public void registerAllSceneObjects(){
+        //int tagNum = 0;
+        gameObjects.Clear();
+        foreach (string tag in tagNames){
+            gameObjects.Add(GameObject.FindGameObjectsWithTag(tag));
+        }
+    }
+    public void displayAllRegisteredObjects(){
+        int tagNum = 0;
+        foreach (GameObject[] objects in gameObjects){
+            foreach(GameObject obj in objects){
+                print(tagNames[tagNum] + ": " + obj);
+            }
+            tagNum+=1;
+        }
+    }
+    void copyNewObject(GameObject existingObject){
+        GameObject newObject = Instantiate( existingObject, 
+                                            existingObject.transform.position, 
+                                            existingObject.transform.rotation);
         
     }
 
